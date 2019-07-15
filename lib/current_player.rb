127c00ed7542a247b45c78bd57280f2f44dc87f8
Ariela Cohen <arielaades@gmail.com>
2019-07-15 14:09:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,14 @@
 def turn_count(board)
   turnCount = 0
   board.each do |space|
-    space == ("X" || "O") ? turnCount += 1 : turnCount 
+    space == ("X" || "O") ? turnCount += 1 : turnCount
   end
   return turnCount
 end
 
 
 def current_player?(board)
-  if turn_count?(board)%2 == 0
+  if turn_count?(board) % 2 == 0
     return "X"
   else
     return "O"
